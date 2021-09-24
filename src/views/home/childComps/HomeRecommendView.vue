0ce9ab9e8481702ea1fe08955edbf2c9450c5d2a
moon-debug <swiliar@163.com>
2021-09-24 14:17:53 +0800

    update

---
+++ src/views/home/childComps/HomeRecommendView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommends" :key="item.title" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeRecommendView",
+  props: {
+    recommends: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  .recommend {
+    display: flex;
+    width: 100%;
+    text-align: center;
+    font-size: 14px;
+    padding: 10px 0 17px;
+    border-bottom: 8px solid #eee;
+  }
+
+  .recommend-item {
+    flex: 1;
+  }
+
+  .recommend-item img {
+    width: 70px;
+    margin-bottom: 10px;
+  }
+</style>
